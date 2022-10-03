eac1ea7ac1090a4b34fd140c984ed90cc6538a90
Do Tri Hai <dotrihai1999@gmail.com>
2022-10-03 10:39:30 +0700

    router and controller

--- src/index.js
+++ src/index.js
@@ -5,8 +5,18 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 //static file
 app.use(express.static(path.join(__dirname, "public")));
+
+//middleware handle form submit (XMLHttpRequest, fetch, axios)
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 
 //http logger
 app.use(morgan("combined"));
@@ -21,9 +31,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+//routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
