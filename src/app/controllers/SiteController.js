f1c14c7ca3019b70df539ac30678c36c6436625e
Do Tri Hai <dotrihai1999@gmail.com>
2022-10-10 14:47:11 +0700

    read from db

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,15 +1,14 @@
 const Course = require("../models/Course");
+const { multipleMongooseToObject } = require("../../util/mongoose");
 
 class SiteController {
   // [GET] /
-  index(req, res) {
-    Course.find({}, function (err, courses) {
-      if (!err) {
-        res.json(courses);
-      } else {
-        res.status(400).json({ error: err });
-      }
-    });
+  index(req, res, next) {
+    Course.find({})
+      .then((courses) => {
+        res.render("home", { courses: multipleMongooseToObject(courses) });
+      })
+      .catch(next); // .catch((err) => next(err));
 
     // res.render("home");
   }
