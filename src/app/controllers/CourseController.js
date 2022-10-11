0492c490d54ea484b6329c0a277a334053caa450
Do Tri Hai <dotrihai1999@gmail.com>
2022-10-11 10:45:03 +0700

    create new course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -2,12 +2,28 @@ const Course = require("../models/Course");
 const { mongooseToObject } = require("../../util/mongoose");
 
 class CourseController {
-  // [GET] / course/:slug
+  // [GET] / courses/:slug
   show(req, res, next) {
     Course.findOne({ slug: req.params.slug })
       .then((course) =>
         res.render("courses/show", { course: mongooseToObject(course) })
       )
+      .catch(next);
+  }
+
+  // [GET] / courses/create
+  create(req, res, next) {
+    res.render("courses/create");
+  }
+
+  // [POST] / courses/store
+  store(req, res, next) {
+    const formData = req.body;
+    formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+    const course = new Course(req.body);
+    course
+      .save()
+      .then(() => res.redirect("/"))
       .catch(next);
   }
 }
