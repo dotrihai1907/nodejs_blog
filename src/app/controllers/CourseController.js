c3e4a6d5f87ed610d94a80682d7804845276d642
haidt10 <dotrihai1999@gmail.com>
2023-02-10 00:23:43 +0700

    soft delete

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -5,9 +5,7 @@ class CourseController {
   // [GET] / courses/:slug
   show(req, res, next) {
     Course.findOne({ slug: req.params.slug })
-      .then((course) =>
-        res.render("courses/show", { course: mongooseToObject(course) })
-      )
+      .then((course) => res.render("courses/show", { course: mongooseToObject(course) }))
       .catch(next);
   }
 
@@ -18,21 +16,18 @@ class CourseController {
 
   // [POST] / courses/store
   store(req, res, next) {
-    const formData = req.body;
-    formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+    req.body.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
     const course = new Course(req.body);
     course
       .save()
-      .then(() => res.redirect("/"))
+      .then(() => res.redirect("/me/stored/courses"))
       .catch(next);
   }
 
   // [GET] / courses/:id/edit
   edit(req, res, next) {
     Course.findById(req.params.id)
-      .then((course) =>
-        res.render("courses/edit", { course: mongooseToObject(course) })
-      )
+      .then((course) => res.render("courses/edit", { course: mongooseToObject(course) }))
       .catch(next);
   }
 
@@ -45,7 +40,21 @@ class CourseController {
 
   // [DELETE] / courses/:id
   delete(req, res, next) {
+    Course.delete({ _id: req.params.id })
+      .then(() => res.redirect("back"))
+      .catch(next);
+  }
+
+  // [DELETE] / courses/:id/force
+  forceDelete(req, res, next) {
     Course.deleteOne({ _id: req.params.id })
+      .then(() => res.redirect("back"))
+      .catch(next);
+  }
+
+  // [PATCH] /courses/:id/restore
+  restore(req, res, next) {
+    Course.restore({ _id: req.params.id })
       .then(() => res.redirect("back"))
       .catch(next);
   }
