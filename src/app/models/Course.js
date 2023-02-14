acfaf03be996ca5d39e8a671e81bafd8aedf01c8
haidt10 <dotrihai1999@gmail.com>
2023-02-14 15:26:58 +0700

    auto increment-id field

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 const slug = require("mongoose-slug-generator");
 const mongooseDelete = require("mongoose-delete");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 
 const Schema = mongoose.Schema;
 
 const CourseSchema = new Schema(
   {
+    _id: { type: Number },
     name: { type: String, required: true },
     description: { type: String },
     image: { type: String },
@@ -13,7 +15,10 @@ const CourseSchema = new Schema(
     videoId: { type: String, required: true },
     level: { type: String },
   },
-  { timestamps: true }
+  {
+    _id: false,
+    timestamps: true,
+  }
 );
 
 //custom query helpers
@@ -29,6 +34,8 @@ CourseSchema.query.sortable = function (req) {
 
 //Add plugins
 mongoose.plugin(slug);
+
+CourseSchema.plugin(AutoIncrement);
 CourseSchema.plugin(mongooseDelete, { deletedAt: true, overrideMethods: "all" });
 
 module.exports = mongoose.model("Course", CourseSchema);
