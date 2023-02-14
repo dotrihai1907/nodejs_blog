4ba83d856cccae5667baf9c8db2a3fb7b6b92285
haidt10 <dotrihai1999@gmail.com>
2023-02-14 14:06:06 +0700

    sort query helper

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-const SortMiddleware = require("./app/middlewares/SortMiddleware");
+const sortMiddleware = require("./app/middlewares/sortMiddleware");
 
 const route = require("./routes");
 const db = require("./config/db");
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(methodOverride("_method"));
 
 //custiom middleware
-app.use(SortMiddleware);
+app.use(sortMiddleware);
 
 //http logger
 app.use(morgan("combined"));
@@ -38,33 +38,10 @@ app.engine(
   "hbs",
   engine({
     extname: ".hbs",
-    helpers: {
-      sum: (a, b) => a + b,
-      sortable: (field, sort) => {
-        const sortType = field === sort.column ? sort.type : "default";
-
-        const icons = {
-          default: "oi oi-elevator",
-          asc: "oi oi-sort-ascending",
-          desc: "oi oi-sort-descending",
-        };
-
-        const types = {
-          default: "desc",
-          asc: "desc",
-          desc: "asc",
-        };
-
-        const icon = icons[sortType];
-        const type = types[sortType];
-
-        return `<a href="?_sort&column=${field}&type=${type}">
-            <span class="${icon}"></span>
-          </a>`;
-      },
-    },
+    helpers: require("./app/helpers/handlebars"),
   })
 );
+
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources", "views"));
 
